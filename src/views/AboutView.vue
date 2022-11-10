4635701b62a65929d7a7fdc4389f9bede434199d
MehakShah <mehakshah9876@gmail.com>
2022-11-10 05:51:45 -0800

    About us screen done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,20 @@
   <DefaultLayout>
     <AboutView>
     </AboutView>
+    <WelcomeSec>
+    </WelcomeSec>
+    <FeaturesAboutSecVue>
+    </FeaturesAboutSecVue>
+    <FunfactSec >
+    </FunfactSec>
   </DefaultLayout>
 </template>
 
 <script setup>
 import DefaultLayout from "../layouts/DefaultLayout.vue";
-import AboutView from "../components/AboutUsSec.vue"
+import AboutView from "../components/AboutUsSec.vue";
+import WelcomeSec from "../components/WelcomeSec.vue";
+import FunfactSec from "../components/FunfactSec.vue";
+import FeaturesAboutSecVue from "../components/FeaturesAboutSec.vue";
+
 </script>
